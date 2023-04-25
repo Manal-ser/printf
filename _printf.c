f79c0656ec0b930924ae4f264d38fe929f3df704
Manal-ser <manal_ser@hotmail.com>
2023-04-25 15:25:09 -0700

    update

---
+++ _printf.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    int num_chars_printed = 0;
+
+    while (format != '\0')
+    {
+        if (format == '%')
+        {
+            format++;
+            switch (format)
+            {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    num_chars_printed++;
+                    break;
+                case 's':
+                    num_chars_printed += printf("%s", va_arg(args, char ));
+                    break;
+                default:
+                    printf("Unknown format specifier: %c",format);
+            }
+        }
+        else
+        {
+            putchar(*format);
+            num_chars_printed++;
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return num_chars_printed;
+}
