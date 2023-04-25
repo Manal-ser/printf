1bf0476ce8b05fc7ae38d22992c66b369601ce8b
Manal-ser <manal_ser@hotmail.com>
2023-04-25 15:39:33 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,40 +1,44 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * _printf - produces output according to a format
+ * @format: character string
+ * Return: the number of characters printed *
+ */
 
 int _printf(const char format, ...)
 {
-    va_list args;
-    va_start(args, format);
+	va_list args;
+	va_start(args, format);
 
-    int num_chars_printed = 0;
+	int num_chars_printed = 0;
 
-    while (format != '\0')
-    {
-        if (format == '%')
-        {
-            format++;
-            switch (format)
-            {
-                case 'c':
-                    putchar(va_arg(args, int));
-                    num_chars_printed++;
-                    break;
-                case 's':
-                    num_chars_printed += printf("%s", va_arg(args, char ));
-                    break;
-                default:
-                    printf("Unknown format specifier: %c",format);
-            }
-        }
-        else
-        {
-            putchar(*format);
-            num_chars_printed++;
-        }
-        format++;
-    }
-
-    va_end(args);
-
-    return num_chars_printed;
+	while (format != '\0')
+	{
+		if (format == '%')
+		{
+			format++;
+			switch (format)
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					num_chars_printed++;
+					break;
+				case 's':
+					num_chars_printed += printf("%s", va_arg(args, char));
+					break;
+				default:
+					printf("Unknown format specifier: %c", format);
+			}
+		}
+		else
+		{
+			putchar(*format);
+			num_chars_printed++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (num_chars_printed);
 }
